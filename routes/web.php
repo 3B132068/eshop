42fa5153c639998df0c6f9da1e55c14c6f7e9feb
donkey <william22850872@gmail.com>
2024-12-21 21:56:42 +0800

    '建立七個路由'

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ProductController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('products', ProductController::class);
+
+/*
+products.index:     GET|HEAD        products                            ProductController@index 列出所有產品
+products.show:      GET|HEAD        products/{product}                  ProductController@show 檢視某一項產品
+products.create:    GET|HEAD        products/create                     ProductController@create 新增產品的表單
+products.store:     POST            products                            ProductController@store 儲存新增的產品
+products.edit:      GET|HEAD        products/{product}/edit             ProductController@edit 修改某一項產品的表單
+products.update:    PUT|PATCH       products/{product}                  ProductController@update 更新某一項產品
+products.destroy:   DELETE          products/{product}                  ProductController@destroy 刪除某一項產品
+*/
