a6e6563606bc53f7bccf6ebc6f5f9c94c20729d2
donkey <william22850872@gmail.com>
2024-12-21 23:10:01 +0800

    '安裝使用者身分驗證相關套件與功能'

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,18 +18,13 @@ use App\Http\Controllers\ProductController;
 Route::get('/', function () {
     return view('welcome');
 });
+
 /*
 Route::resource('products', ProductController::class)->only([
     'index', 'show', 'store', 'update', 'destroy'
 ]);
 */
-Route::get('products', [ProductController::class, 'index'])->name('products.index');
-Route::get('products/{product}', [ProductController::class, 'show'])->name('products.show');
-Route::get('products/create', [ProductController::class, 'create'])->name("products.create");
-Route::post('products', [ProductController::class, 'store'])->name("products.store");
-Route::get('products/{products}/edit', [ProductController::class, 'edit'])->name("products.edit");
-Route::patch('products/{product}', [ProductController::class, 'update'])->name("products.update");
-Route::delete('products/{product}', [ProductController::class, 'destroy'])->name("products.destroy");
+
 /*
 products.index:     GET|HEAD        products                            ProductController@index 列出所有產品
 products.show:      GET|HEAD        products/{product}                  ProductController@show 檢視某一項產品
@@ -37,3 +34,15 @@ products.edit:      GET|HEAD        products/{product}/edit             ProductC
 products.update:    PUT|PATCH       products/{product}                  ProductController@update 更新某一項產品
 products.destroy:   DELETE          products/{product}                  ProductController@destroy 刪除某一項產品
 */
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
